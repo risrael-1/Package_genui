345208e86a92d063ce3a5863e8654d26708170ce
Rapha <rapha.israel@live.fr>
2022-01-18 22:14:18 +0100

    add project

---
+++ Sources/GENUI/GENUI.swift
@@ -0,0 +1,8 @@
+public struct GENUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+
---
+++ Tests/GENUITests/GENUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GENUI
+
+final class GENUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GENUI().text, "Hello, World!")
+    }
+}
