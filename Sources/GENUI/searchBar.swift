fd6b6dd490ea951b1d04ba3d0cb8e879119b5b95
Rapha <rapha.israel@live.fr>
2022-01-30 12:08:36 +0100

    add searchBar

--- Sources/GENUI/searchBar.swift
+++ Sources/GENUI/searchBar.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+public class SearchBarUI {
+    
+    public init() {
+        
+    }
+    
+    public func add(x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat, placeholer: String?) -> UISearchBar {
+        let searchBar = UISearchBar()
+        searchBar.frame = CGRect(x: x, y: y, width: w, height: h)
+        searchBar.placeholder = placeholer ?? ""
+        return searchBar
+    }
+}
