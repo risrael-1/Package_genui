7478eb1f26ab69c87255efa411039c636e35bfdd
Rapha <rapha.israel@live.fr>
2022-03-06 15:00:58 +0100

    fix

--- Sources/GENUI/switch.swift
+++ Sources/GENUI/switch.swift
@@ -108,29 +108,31 @@ import UIKit
     }
     
     private func setUp() {
-            
+
         self.subviews.forEach({ subview in
-           subview.removeFromSuperview()
+            subview.removeFromSuperview()
         })
-        
+
         self.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: self.frame.size.width, height: self.frame.size.height)
         self.backgroundColor = .clear
         let backgroundSwitch = UIView.init(frame: self.frame)
         backgroundSwitch.backgroundColor = self.switchColor
         backgroundSwitch.clipsToBounds = true
-        backgroundSwitch.frame = CGRect(x: 0, y: 0, width:  self.frame.size.width, height: bounds.size.height)
-        
+        backgroundSwitch.frame = CGRect(x: 0, y: 0, width:  self.frame.size.width, height: self.frame.size.height)
+
         self.addSubview(backgroundSwitch)
-        
+
         switchui.tintColor = self.switchColorOff
         switchui.onTintColor = self.switchColorOn
-        
+
         label.text = self.labelDescription
         label.textColor = self.textColor
-        label.frame = CGRect(x: 20, y: bounds.size.height / 2 - self.label.frame.size.height / 2 , width: bounds.size.width / 2, height: bounds.size.height)
+        label.frame =  CGRect(x: 20, y: bounds.size.height / 2 - self.label.frame.size.height / 2 , width: bounds.size.width / 2, height: bounds.size.height)
         label.numberOfLines = 0
         self.addSubview(label)
-        
+
+        switchui.frame =  CGRect(x: self.frame.size.width - 110, y: self.frame.size.height / 2 - 20  , width: 40, height: 40)
+        self.addSubview(switchui)
         self.superview?.addSubview(self)
     }
     
